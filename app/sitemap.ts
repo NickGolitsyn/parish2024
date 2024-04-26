b9c8ce46a9fa11042257d40ccd4fbcc2aa61b96f
Nick Golitsyn <45421041+NickGolitsyn@users.noreply.github.com>
2024-04-26 09:39:44 +0100

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,12 @@
 export default async function sitemap() {
-  const url = 'https://parish2024.vercel.app';
+  const url = 'https://parohianorwich.org';
   const languages = ['en', 'ro'];
   const pages = ['', '/about', '/contact', '/services', '/gallery', '/donate', '/sunday-school'];
 
   const sitemapEntries = languages.flatMap((lang) =>
     pages.map((page) => ({
       url: `${url}/${lang}${page}`,
-      lastModified: '2024-04-25',
+      lastModified: '2024-04-26',
     }))
   );
 
